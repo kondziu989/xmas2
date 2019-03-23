f86102bef024f2e6ca46b8582536a6fa3e4cd6c4
Konrad <kondziu989@gmail.com>
2019-03-23 14:37:01 +0100

    Basic setup

--- app/src/main/java/com/zpi/xmas/MainActivity.kt
+++ app/src/main/java/com/zpi/xmas/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.zpi.xmas
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        events_btn.setOnClickListener {
+            startActivity(Intent(this, Events::class.java));
+        }
     }
 }
